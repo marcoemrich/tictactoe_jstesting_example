731edf524c76c3d8fb872b5d628dcee10bb5662b
Marco Emrich <marco.emrich@codecentric.de>
2020-06-25 14:19:33 +0200

    remove old enzyme state example

--- src/__index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {Foo} from './foo'
-// import {Board, Cell, TicTacToe} from './tic_tac_toe'
-
-
-const App = document.getElementById("app");
-
-ReactDOM.render(<Foo name="Salzburg" />, App);
